0a23609e0970e2d0254789f5985bb8375e93c923
Benie140902 <148710852+Benie140902@users.noreply.github.com>
2025-07-14 19:07:16 +0530

    Add files via upload

---
+++ Transmitter/main.h
@@ -0,0 +1,9 @@
+#include <vector>
+
+extern std::vector<int>msg;
+extern int num_bits;
+std::vector <int> crc_append24a(const std:: vector<int>& input_codeblock);
+std::vector <int> crc_append24b(const std:: vector<int>& input_codeblock);
+std::vector <int> crc_append16(const std:: vector<int>& input_codeblock);
+
+
