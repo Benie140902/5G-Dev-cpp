e9fe3bff38e941adac76ea81c6553a9ca50b63f7
Benie140902 <148710852+Benie140902@users.noreply.github.com>
2025-07-14 17:56:27 +0530

    Add files via upload

---
+++ Transmitter/5G NR Encoder/segment.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include<cmath>
+#include "/home/mantiswave/5G_DEV_CPP/Transmitter/main.h"
+
+using namespace std;
+
+const int MAX_Block_Size = 8448; // Maximum block size for encoding according to 3GPP TS 38.212
