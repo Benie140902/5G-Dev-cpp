2f0556eb6fcb3cd7b820aed100d6ac9fbea62c04
Benie140902 <148710852+Benie140902@users.noreply.github.com>
2025-07-18 08:06:55 +0530

    Update main.h

--- Transmitter/main.h
+++ Transmitter/main.h
@@ -6,4 +6,9 @@ std::vector <int> crc_append24a(const std:: vector<int>& input_codeblock);
 std::vector <int> crc_append24b(const std:: vector<int>& input_codeblock);
 std::vector <int> crc_append16(const std:: vector<int>& input_codeblock);
 
+namespace segment{
+std::vector<std::vector<int>>segment_codeblock(const std::vector<int>& transport_codeblock,int block_size);
+int ldpc_basegraph_select(int num_tb_bits)
+}
+
 
